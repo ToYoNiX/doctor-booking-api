4351207eed25c6c21d4fc08d0fb0a1b13c158605
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-08-20 23:31:20 +0300

    fixing the order of middlewares.

--- index.js
+++ index.js
@@ -3,30 +3,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-
 // Secrets
 require("dotenv").config()
 const PORT = process.env.PORT
 const connString = process.env.connString
 
-
 // Creating backend app
 const app = express()
 
-
-// Middleware
+// Global Middleware
 app.use(express.json())
-
-app.use(function (req, res) {
-  res.status(500).json("internal server error");
-});
-
-app.use(cors({ origin: "*" }));
-
-app.use((req, res) => {
-  res.status(404).json({ message: "not found" });
-});
-
+app.use(cors({ origin: "*" }))
 
 // Routes
 const userRoutes = require("./routes/user.js");
@@ -41,6 +28,16 @@ app.use("/api/clinics", clinicRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/reviews", reviewRoutes);
 
+// Not Found Middleware (after routes)
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Not found" });
+});
+
+// Error Handler Middleware (last one)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Internal server error" });
+});
 
 // Database connections
 mongoose.connect(connString).then(() => {
